6265cfa3c7016e081222320990b6903310ab6e2e
ArShuranov <arshuranov@yandex.ru>
2023-01-20 18:07:45 +0700

    Делаем ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет мир");
+        System.out.println("Пока все работает очень странно и не совсем так, как в уроке, но вроде все получилось");
     }
 }
